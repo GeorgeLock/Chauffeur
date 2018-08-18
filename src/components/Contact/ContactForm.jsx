1ef94e49967aa45b42a733951384b70a1eb80a74
George Lock <lockgeorge.web.design@gmail.com>
2018-08-18 11:44:35 +0100

    Contact Form added

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const ContactForm = () => {
   return (
     <div>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
+      <form name="contact" method="POST" data-netlify>
         <input name="name" placeholder="John Doe" type="text" />
         <button>Send</button>
       </form>
