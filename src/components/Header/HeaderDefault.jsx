4abd86c6e1e9e906ebcbc7c683e6332a447082f2
George Lock <lockgeorge.web.design@gmail.com>
2018-08-18 11:22:41 +0100

    Contact Form added

--- src/components/Header/HeaderDefault.jsx
+++ src/components/Header/HeaderDefault.jsx
@@ -6,26 +6,25 @@ class HeaderDefault extends Component {
   render() {
     const { localTitle } = this.props;
 
-    let top = '0';
-    let scale;
+    // let top = '0';
+    // let scale;
+    // const content = document.getElementsByClassName('main-container');
 
-    if (localTitle === 'Home' || localTitle === 'home') {
-      const content = document.getElementsByClassName('main-container');
-      this.top = '0';
-      this.scale = 0;
-      for (let i = 0; i < content.length; i++) {
-        content[i].style.paddingTop = '93vh';
-      }
-    } else {
-      const content = document.getElementsByClassName('main-container');
-      this.top = '-1000px';
-      this.scale = 0;
-      for (let i = 0; i < content.length; i++) {
-        content[i].style.paddingTop = '10vh';
-      }
-    }
+    // if (localTitle === 'Home' || localTitle === 'home') {
+    //   this.top = '0';
+    //   this.scale = 0;
+    //   for (let i = 0; i < content.length; i++) {
+    //     content[i].style.paddingTop = '93vh';
+    //   }
+    // } else {
+    //   this.top = '-1000px';
+    //   this.scale = 0;
+    //   for (let i = 0; i < content.length; i++) {
+    //     content[i].style.paddingTop = '10vh';
+    //   }
+    // }
     return (
-      <div className="header" style={{ top: this.top, scale: this.scale }}>
+      <div className="header" style={}>
         <img
           className="header-image"
           src={HeaderImage}
