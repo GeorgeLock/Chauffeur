2fa6cc64b5f881ce5db77ecca2c18b6819e18d5d
George <lockgeorge.web.design@gmail.com>
2018-08-25 12:05:26 +0100

    Contact Form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-
+import Label from "./Label";
 import { TextField } from "react-md";
+import "./bootstrap.min.css";
 import "./ContactForm.scss";
 
 class ContactForm extends Component {
@@ -16,62 +17,47 @@ class ContactForm extends Component {
   }
 
   render() {
-    const content = this.state.checked
-      ? // <Label
-        //   fieldName="Phone"
-        //   type="tel"
-        //   id="phone"
-        //   placeholder="Home / Mobile"
-        //   className="pr-2 fas fa-phone text-info"
-        //   required
-        // />
-        null
-      : null;
+    const content = this.state.checked ? (
+      <Label
+        fieldName="Phone"
+        type="tel"
+        id="phone"
+        placeholder="Home / Mobile"
+        className="pr-2 fas fa-phone text-info"
+        required
+      />
+    ) : null;
 
     return (
-      <form
-        name="contact-form"
-        method="POST"
-        data-netlify-honeypot="bot-field"
-        data-netlify="true"
-      >
-        <div className="md-grid">
-          <p class="hidden">
-            <label>
-              <input type="hidden" name="bot-field" />
-            </label>
-          </p>
-
-          <TextField
-            id="floating-center-title"
-            label="Title"
-            lineDirection="center"
-            placeholder="Hello World"
-            className="md-cell md-cell--bottom"
-            name="Name"
-          />
-          <TextField
-            id="floating-multiline"
-            label="Email"
-            lineDirection="right"
-            placeholder="example@example.com"
-            className="md-cell md-cell--bottom"
-            name="Email"
-            type="email"
-          />
-          <TextField
-            id="floating-multiline"
-            label="Number of Passengers"
-            type="number"
-            className="md-cell md-cell--bottom"
-            name="Number of Passengers"
-            placeholder="1"
-          />
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
+      <div id="contact">
+        <form
+          name="contact-form"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          action="#"
+          className="contact-form"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="md-grid">
+            <TextField
+              id="floating-center-title"
+              label="Passenger Name"
+              lineDirection="center"
+              placeholder="John Doe"
+              name="Name"
+              className="md-cell md-cell--bottom"
+            />
+          </div>
+        </form>
+      </div>
     );
   }
 }
 
 export default ContactForm;
+{
+  /* <form
+
+> */
+}
